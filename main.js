6ac2eb0ddf0cab0e55199d4dd4f08e7e5befd40a
Coder8986 <123604652+Coder8986@users.noreply.github.com>
2023-08-15 19:31:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+    
+}
+
+function draw() {
+    
+}
+
+function setup(){
+    canvas = createCanvas(500,400);
+    canvas.center();
+}
+
+
+function Take_snapshot(){
+    save("img.png");
+}
